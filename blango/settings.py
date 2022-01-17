02ac1a82a2add2750f860b6616673ab5614874a0
Horst Gassner <kontakt@horst-gassner.com>
2022-01-17 16:12:23 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -24,6 +24,12 @@ class Dev(Configuration):
   ACCOUNT_ACTIVATION_DAYS = 7
   REGISTRATION_OPEN = True
 
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
+
   # Quick-start development settings - unsuitable for production
   # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -50,12 +56,17 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      "django.contrib.sites",
       'django.contrib.staticfiles',
       'blango_auth',
       "blog",
       'crispy_forms',
       "crispy_bootstrap5",
       "debug_toolbar",
+      "allauth", 
+      "allauth.account", 
+      "allauth.socialaccount", 
+      "allauth.socialaccount.providers.google"
   ]
 
   MIDDLEWARE = [
--- blango/urls.py
+++ blango/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path('admin/', admin.site.urls),
     path("ip/", blog.views.get_ip),
